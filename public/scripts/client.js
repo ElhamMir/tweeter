4d42bdce12410139ef04a480f0892fbeb3fad2c6
Elham <mireshghi@yahoo.com>
2021-11-18 23:44:35 +0000

    add data to page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,60 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function () {
+    const data = [
+        {
+          "user": {
+            "name": "Newton",
+            "avatars": "https://i.imgur.com/73hZDYK.png"
+            ,
+            "handle": "@SirIsaac"
+          },
+          "content": {
+            "text": "If I have seen further it is by standing on the shoulders of giants"
+          },
+          "created_at": 1461116232227
+        },
+        {
+          "user": {
+            "name": "Descartes",
+            "avatars": "https://i.imgur.com/nlhLi3I.png",
+            "handle": "@rd" },
+          "content": {
+            "text": "Je pense , donc je suis"
+          },
+          "created_at": 1461113959088
+        }
+      ]
 
+const createTweetElement = function(tweet) {
+    let $tweet = `
+      <article class="tweet">
+          <div>${tweet.user.avatars}</div>
+          <div>${tweet.user.name}</div>
+          <div>${tweet.user.handle}</div>
+          <div>${tweet.content.text}</div>
+          <hr>
+          <footer>${timeago.format(new Date(tweet.created_at))}</footer>
+          </article>`;
+  
+  
+      console.log(tweet)
+    return $tweet;
+  }
+
+//createTweetElement(data)
+
+const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+        console.log(tweet,"here")
+        $("#tweets-container").append(createTweetElement(tweet));
+
+    }
+   
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+}
+
+
+
+renderTweets(data);
+})
